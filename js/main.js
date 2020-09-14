d298e10c66f8bc782f4960f24856ce0109d53981
Alexander Celis <dacelis0@misena.edu.co>
2020-09-14 18:54:39 -0500

    bugs fixed

--- js/main.js
+++ js/main.js
@@ -5,18 +5,14 @@ accordion.addEventListener("click", (e) => {
     ds = element.dataset;
 
   if (ds.toggle === "collapse") {
-    if (element.classList.contains("arrow-down")) {
-      element.classList.toggle("arrow-up");
+    
+    const current = accordion.querySelector(".btn.active");
+    if (current && current !== element) {
+      current.classList.toggle("active");
     }
 
-    /* const path = accordion.getElementsByClassName("btn");
-    for (const iterator of path) {
-      let index = iterator.classList.value.includes("arrow-down");
-      if (index === true) {
-        iterator.classList.replace("arrow-down", "arrow-up");
-      } else {
-        iterator.classList.replace("arrow-up", "arrow-down");
-      }
-    } */
+    if (element.classList.contains("btn")) {
+      element.classList.toggle("active");
+    }
   }
 });
