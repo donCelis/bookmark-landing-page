6811f2cf07132ab2e2e8253ef937de205ea0b15a
Alexander Celis <dacelis0@misena.edu.co>
2020-09-15 16:41:07 -0500

    upgrade code conditions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-const accordion = document.querySelector(".questions .accordion");
+const selector = document.querySelector.bind(document);
+
+const accordion = selector(".questions .accordion");
 
 accordion.addEventListener("click", (e) => {
   const element = e.target,
     ds = element.dataset;
 
   if (ds.toggle === "collapse") {
-    
     const current = accordion.querySelector(".btn.active");
-    if (current && current !== element) {
-      current.classList.toggle("active");
-    }
 
-    if (element.classList.contains("btn")) {
-      element.classList.toggle("active");
-    }
+    current && current !== element && current.classList.toggle("active");
+
+    element.classList.contains("btn") && element.classList.toggle("active");
   }
 });
